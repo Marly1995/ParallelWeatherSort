31ebcc74997a5bdddddda6a58d596ad5122ffb2c
Marlon Gilliam <14471455@students.lincoln.ac.uk>
2017-03-29 23:08:15 +0100

    added comments mk1

--- Tutorial 3/Tutorial 3.cpp
+++ Tutorial 3/Tutorial 3.cpp
@@ -21,6 +21,11 @@ void print_help() {
 	std::cerr << "  -l : list all platforms and devices" << std::endl;
 	std::cerr << "  -h : print this message" << std::endl;
 }
+
+// File parse using fscanf efficient during debug but less so in release.
+// Data stored direct into float array or int array
+// time debug: ~9s
+// time release: ~2.5s
 float* fscanFile(char* fileDirectory, int dataSize)
 {
 	float* data = new float[dataSize];
@@ -30,12 +35,16 @@ float* fscanFile(char* fileDirectory, int dataSize)
 	fseek(myFile, 0L, SEEK_SET);
 	for (int i = 0; i < dataSize; i++)
 	{
-		fscanf(myFile, "%*s %*lf %*lf %*lf %*lf %f", &data[i]);
+		fscanf(myFile, "%*s %*lf %*lf %*lf %*lf %f", &data[i]); // values with a star are read but not stored
 	}
 	fclose(myFile);
 	return data;
 }
 
+// File reading using ifstream slow in debug but fast in release
+// Data stored into single string
+// time debug: ~30s
+// time release: ~1s
 string loadFile(char* fileDirectory)
 {
 	ifstream file;
@@ -57,6 +66,7 @@ string loadFile(char* fileDirectory)
 	return data;
 }
 
+// File parse for data in single string
 vector<float> parseData(string data, int dataSize)
 {
 	vector<float> temps;
@@ -82,6 +92,7 @@ vector<float> parseData(string data, int dataSize)
 	return temps;
 }
 
+// Function to print results
 void printData(string info, float result, cl::Event event)
 {
 	std::cout << "///////////////////////////////////////////////////////////////////////////////" << std::endl;
@@ -95,7 +106,6 @@ void printData(string info, float result, cl::Event event)
 }
 
 int main(int argc, char **argv) {
-	//Part 1 - handle command line options such as device selection, verbosity, etc.
 	int platform_id = 0;
 	int device_id = 0;
 
@@ -142,7 +152,7 @@ int main(int argc, char **argv) {
 		}
 		}*/
 		time = clock() - time;
-		std::cout << "read and parse time = " << time << std::endl;
+		std::cout << "Time to read and parse file [s]: " << time << std::endl;
 
 		//Part 2 - host operations
 		//2.1 Select computing devices
@@ -156,9 +166,7 @@ int main(int argc, char **argv) {
 
 		//2.2 Load & build the device code
 		cl::Program::Sources sources;
-
 		AddSources(sources, "../../Tutorial 3/my_kernels3.cl");
-
 		cl::Program program(context, sources);
 
 		//build and debug the kernel code
@@ -172,38 +180,12 @@ int main(int argc, char **argv) {
 			throw err;
 		}
 
+		// define types for easy alteration of multiple functions
 		typedef int mytype;
 		typedef float myfloat;
 
-		std::vector<mytype> A(dataSize, 0);
-		std::vector<mytype> I(dataSize, 0);
-		std::vector<myfloat> FA(dataSize, 0);
-		for (int i = 0; i < dataSize; i++)
-		{
-			I[i] = data[i];
-			A[i] = data[i]*100; 
-			FA[i] = data[i];
-		}
-		time = clock() - time;
-
-		size_t local_size = 32;
-
-		size_t padding_size = A.size() % local_size;
-
-		if (padding_size) {
-			//create an extra vector with neutral values
-			std::vector<int> A_ext(local_size-padding_size, 0);
-			std::vector<float> FA_ext(local_size - padding_size, 0);
-			//append that extra vector to our input
-			A.insert(A.end(), A_ext.begin(), A_ext.end());
-			FA.insert(FA.end(), FA_ext.begin(), FA_ext.end());
-		}
-
-		size_t input_elements = A.size();//number of input elements
-		size_t input_size = A.size()*sizeof(mytype);//size in bytes
-		size_t finput_size = FA.size() * sizeof(myfloat);
-		size_t nr_groups = input_elements / local_size;
-
+		// events used for profilig differnt kernal operations
+		// events marked with an f for floating operations
 		cl::Event max_event;
 		cl::Event fmax_event;
 		cl::Event min_event;
@@ -217,44 +199,101 @@ int main(int argc, char **argv) {
 		cl::Event sort_event;
 		cl::Event fsort_event;
 
+		// Create vectors to store data for operations
+		// Different vector for: floats, integers and multliplied integers for approximate float values
+		std::vector<mytype> A(dataSize, 0);
+		std::vector<mytype> I(dataSize, 0);
+		std::vector<myfloat> FA(dataSize, 0);
+		// load all data into vectors
+		for (int i = 0; i < dataSize; i++)
+		{
+			I[i] = data[i];
+			A[i] = data[i]*100; 
+			FA[i] = data[i];
+		}
+		time = clock() - time;
+
+		// define a workgroup size
+		// 32 used as it is fastest with current operations for this data type
+		// 32 is also optimal for use on the gtx platform
+		// TODO: reasons for this
+		size_t local_size = 32;
+
+		// padding given by the remainer of number of elements when split into work groups
+		size_t padding_size = A.size() % local_size;
+
+		if (padding_size) {
+			// create empty vector with size of the remaining space in the last workgroup
+			// TODO: fill plain integer vector
+			std::vector<int> A_ext(local_size-padding_size, 0);
+			std::vector<float> FA_ext(local_size - padding_size, 0);
+			// add this vector to the end of our data
+			A.insert(A.end(), A_ext.begin(), A_ext.end());
+			FA.insert(FA.end(), FA_ext.begin(), FA_ext.end());
+		}
+
+		size_t input_elements = A.size(); // number of elements in data set including padding
+		size_t input_size = A.size()*sizeof(mytype); // size in bytes of the data set (integers)
+		size_t finput_size = FA.size() * sizeof(myfloat); // size in bytes of the data set (floats)
+		size_t nr_groups = input_elements / local_size; // number of work groups 
+
 		//host - output
-		//int *B;
-		//size_t output_size = sizeof(int);
-		std::vector<mytype> B(1);		
-		size_t output_size = B.size()*sizeof(mytype);//size in bytes
-		std::vector<myfloat> FB(1);
-		size_t foutput_size = B.size() * sizeof(myfloat);//size in bytes
-		std::vector<myfloat> FC(input_elements);
-		size_t fmean_output_size = FC.size() * sizeof(myfloat);
+		// TODO: redefine single values as pointers not vectors
+		// Values to store outputs 
+		std::vector<mytype> B(1);	
 		std::vector<mytype> C(input_elements);
 		std::vector<mytype> D(input_elements);
+		std::vector<myfloat> FB(1);
+		std::vector<myfloat> FC(input_elements);		
 		std::vector<myfloat> FD(input_elements);
+		// Sizes of outputs in bytes	
+		size_t output_size = B.size()*sizeof(mytype); 
+		size_t foutput_size = B.size() * sizeof(myfloat);
+		// TODO: think this can go
+		size_t fmean_output_size = FC.size() * sizeof(myfloat);
+		
 		
 		
 
 		//device - buffers
+		// create all buffers at start
+		// buffers for integer operations
+		// buffers to store dataset in integers
 		cl::Buffer buffer_A(context, CL_MEM_READ_ONLY, input_size);
+		cl::Buffer buffer_I(context, CL_MEM_READ_ONLY, input_size);
+
+		// buffers to accept integer outputs
 		cl::Buffer buffer_B(context, CL_MEM_READ_WRITE, output_size);
 		cl::Buffer buffer_C(context, CL_MEM_READ_WRITE, input_size);
 		cl::Buffer buffer_D(context, CL_MEM_READ_WRITE, input_size);
-		cl::Buffer buffer_I(context, CL_MEM_READ_ONLY, input_size);
-
+		
+		// buffer to store dataset in floats
 		cl::Buffer buffer_FA(context, CL_MEM_READ_ONLY, finput_size);
+
+		// buffers to accept float outputs
 		cl::Buffer buffer_FB(context, CL_MEM_READ_WRITE, foutput_size);
 		cl::Buffer buffer_FC(context, CL_MEM_READ_WRITE, fmean_output_size);
 		cl::Buffer buffer_FD(context, CL_MEM_READ_WRITE, finput_size);
 
+		// queue all buffers prior to running kernals
+		// queue write buffers for integer values
 		queue.enqueueWriteBuffer(buffer_A, CL_TRUE, 0, input_size, &A[0]);
 		queue.enqueueWriteBuffer(buffer_I, CL_TRUE, 0, input_size, &I[0]);
+
+		// queue fill and read buffers for integer values
 		queue.enqueueFillBuffer(buffer_B, 0, 0, output_size);
 		queue.enqueueReadBuffer(buffer_C, 0, 0, input_size, &C[0]);
 		queue.enqueueReadBuffer(buffer_D, 0, 0, input_size, &D[0]);
 
+		// queue write buffers for float values
 		queue.enqueueWriteBuffer(buffer_FA, CL_TRUE, 0, finput_size, &FA[0]);
+
+		// queue fill and read buffers for float values
 		queue.enqueueFillBuffer(buffer_FB, 0, 0, foutput_size);
 		queue.enqueueFillBuffer(buffer_FC, 0, 0, fmean_output_size);
 		queue.enqueueReadBuffer(buffer_FD, CL_TRUE, 0, finput_size, &FD[0]);
 
+		// TODO: make all of these functions
 		// integer maximum	
 		cl::Kernel kernel_0 = cl::Kernel(program, "reduce_max");
 		kernel_0.setArg(0, buffer_A);
